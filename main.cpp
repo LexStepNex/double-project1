313c48e74517f6a21e3fbfa1b2e53542ed7e774b
LexStepNex <168106286+LexStepNex@users.noreply.github.com>
2025-02-21 23:44:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "iostream"
 
 int main() {
-    std::cout << "Hello world\n";
+    std::cout << "Hello Drujhok\n";
     std::cout << "Hello world\n";
     std::cout << "Dead world\n";
     std::cout << "Dead world\n";
